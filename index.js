b6e3f61848a88ae46b91ddf4c6e67fe948d61905
Jelena <jelena.durovic@powerhrg.com>
2021-05-23 01:31:14 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    const newArray = [drivers[0], drivers[1]]
+     return newArray
+}
+
+function returnLastTwoDrivers(drivers){
+    const newArray = [drivers[2], drivers[3]]
+    return newArray
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+function createFareMultiplier(name){
+    return function(){return name * name}
+
+}
+
+function fareDoubler(argument){
+    return argument * 2
+}
+
+function fareTripler(command){
+    return command * 3
+
+}
+
+function selectDifferentDrivers(arrayOfDrivers, secondArgument){
+    return secondArgument(arrayOfDrivers)
+    
+}
